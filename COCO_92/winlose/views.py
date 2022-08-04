94e27b6c7e7d4e009e6f9b131bc42a9aaca70257
SeanKim37 <suminkim1317@g.skku.edu>
2022-08-04 14:53:42 +0900

    add winlose DNN model django

--- COCO_92/winlose/views.py
+++ COCO_92/winlose/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,9 +9,13 @@ from rest_framework.views import APIView
 
 class WinningRate(APIView):
     def get(self, request):
+
+        # win_predict = os.path.dirname("C:/COCO_AI_Web_Hackaton/") + "\DNN_winningRate_model.h5"
+        # winlose_model = tf.keras.models.load_mdel(win_predict)
         return render(request, "winlose/winningRate.html")
 
 
 class UploadWar92(APIView):
     def post(self, request):
         return Response(status=200)
+
